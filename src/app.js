457ed635647c070ce115268add2493c429d2fc08
damirdine <damirdine@outlook.fr>
2023-09-12 14:42:45 +0200

    add data

--- src/app.js
+++ src/app.js
@@ -11,14 +11,45 @@ nunjucks.configure("src/views", {
   watch: true,
 });
 
+const data = [
+  {
+    id:1,
+    title : "hell",
+    content: "loresm skjskjsjffoissjsjf ofdsis",
+    comments:[
+      {
+        user_name: "random",
+        message:"helllo",
+        responses: [{user_name: "replayer",message:"Welcom"}]
+      }
+    ]
+  },
+  {
+    id:2,
+    title : "hell2",
+    content: "loresm skjskjsjffoissjsjf ofdsis",
+    comments:[
+      {
+        user_name: "random",
+        message:"helllo",
+        responses: [{user_name: "replayer",message:"Welcom"}]
+      }
+    ]
+  }
+];
 app.get("/", (req, res) => {
-  res.render("pages/home.njk", { title: "hello" });
+  res.render("pages/home.njk", { title: "hello", data });
+});
+
+app.get("/article/:id",(req, res) => {
+  const articleId = req.params.id;
+  res.send(`Article ID: ${articleId}`);
 });
 
 app.get("/load-data", (req, res) => {
   // Replace with code to fetch data or perform actions here
   const newData = "This is the dynamically loaded content.";
-  res.render("components/message.njk", { message: newData });
+  res.send( newData);
 });
 
 module.exports = app;
