4d5ff35d489c4a5edad24e54fe97ed57abf5125a
damirdine <damirdine@outlook.fr>
2023-09-15 14:01:02 +0200

    config keycloak login

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-// app.js
+require("dotenv").config();
 const express = require("express");
-const session = require('express-session');
-const Keycloak = require('keycloak-connect');
+const session = require("express-session");
+const cors = require("cors");
+const Keycloak = require("keycloak-connect");
 
 const nunjucks = require("nunjucks");
 
@@ -15,50 +16,85 @@ nunjucks.configure("src/views", {
 
 const data = [
   {
-    id:1,
-    title : "hell",
+    id: 1,
+    title: "hell",
     content: "loresm skjskjsjffoissjsjf ofdsis",
-    comments:[
+    comments: [
       {
         user_name: "random",
-        message:"helllo",
-        responses: [{user_name: "replayer",message:"Welcom"}]
-      }
-    ]
+        message: "helllo",
+        responses: [{ user_name: "replayer", message: "Welcom" }],
+      },
+    ],
   },
   {
-    id:2,
-    title : "hell2",
+    id: 2,
+    title: "hell2",
     content: "loresm skjskjsjffoissjsjf ofdsis",
-    comments:[
+    comments: [
       {
         user_name: "random",
-        message:"helllo",
-        responses: [{user_name: "replayer",message:"Welcom"}]
-      }
-    ]
-  }
+        message: "helllo",
+        responses: [{ user_name: "replayer", message: "Welcom" }],
+      },
+    ],
+  },
 ];
-app.use(express.static(__dirname +'/../public'));
+// usr cors
+app.use(cors({ origin: "*" }));
+// set up keycloak auth & session
+const sessionMemoryStore = new session.MemoryStore();
+app.use(
+  session({
+    secret: "votre_secret_de_session",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionMemoryStore,
+  })
+);
+const kcConfig = {
+  realm: process.env.KC_REALM,
+  "auth-server-url": process.env.KC_SERVER_URL,
+  resource: process.env.KC_RESOURCE,
+  // "ssl-required": "external",
+  // "public-client": true,
+  // "confidential-port": 0
+};
+const keycloak = new Keycloak({ store: sessionMemoryStore }, kcConfig);
+
+app.use(keycloak.middleware());
 
 // set default view engine
-app.set('view engine', 'html');
+app.use(express.static(__dirname + "/../public"));
+app.set("view engine", "html");
 
-app.get("/", (req, res) => {
-  res.render("pages/home", { title: "hello", data });
+app.get("/", async (req, res) => {
+  const user = req.kauth.grant?.access_token.content;
+  console.log(user);
+  res.render("pages/home", { title: "hello", data, user });
 });
 
-app.get("/article/:id",(req, res) => {
+app.get("/article/:id", keycloak.protect(), (req, res) => {
+  const user = req.kauth.grant?.access_token.content;
+
   const articleId = req.params.id;
-  const article = data.find(el => el.id==articleId)
-  if (!article) return res.render("pages/404", {message:"Record "+articleId + " not found"});
-  res.render("pages/article", { data: article});
+  const article = data.find((el) => el.id == articleId);
+  if (!article)
+    return res.render("pages/404", {
+      message: "Record " + articleId + " not found",
+    });
+  res.render("pages/article", { data: article, user });
+});
+
+//// Login / Logout
+app.get("/login", keycloak.protect(), (req, res) => {
+  res.redirect("/");
 });
 
 app.get("/load-data", (req, res) => {
   // Replace with code to fetch data or perform actions here
   const newData = "This is the dynamically loaded content.";
-  res.send( newData);
+  res.send(newData);
 });
 
 module.exports = app;
